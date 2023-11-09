144e7bae3f9fb7d7afdef4ebf1bd34129d9f4bfc
hayley-alcorn <137900064+hayley-alcorn@users.noreply.github.com>
2023-11-09 17:06:19 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+function changeColor1() {
+    var elements = document.getElementsByClassName("colorchange");
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor = "#9bedff";
+    } 
+    document.getElementById("name").style.color = "#9bedff"
+    document.getElementById("footer").style.color = "#9bedff"
+    document.getElementById("body").style.backgroundColor = "rgb(208, 244, 255)"
+
+}
+
+function changeColor2() {
+    var elements = document.getElementsByClassName("colorchange");
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor = "#FEC5E5";
+    } 
+    document.getElementById("name").style.color = "#FEC5E5"
+    document.getElementById("footer").style.color = "#FEC5E5"
+    document.getElementById("body").style.backgroundColor = "#FADDE2"
+
+}
+
+function changeColor3() {
+    var elements = document.getElementsByClassName("colorchange");
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor = "#D8B9FF";
+    } 
+    document.getElementById("name").style.color = "#D8B9FF"
+    document.getElementById("footer").style.color = "#D8B9FF"
+    document.getElementById("body").style.backgroundColor = "#E6D7FF"
+
+}
+
+function changeColor4() {
+    var elements = document.getElementsByClassName("colorchange");
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor = "#83F28F";
+    } 
+    document.getElementById("name").style.color = "#83F28F"
+    document.getElementById("footer").style.color = "#83F28F"
+    document.getElementById("body").style.backgroundColor = "#CEFAD0"
+
+}
+
